356044c2775f93d7b745a6b3d3f9b57def45f82c
erinlevine <erinclevine@gmail.com>
2017-05-29 12:22:47 -0400

    It is done with all fresh code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,115 +1,112 @@
 $(document).ready(function() {
 
-
-	crystals = ['assets/images/red.png','assets/images/blue.png','assets/images/yellow.png','assets/images/green.png'];
-
-	//Create variable for wins
+//Setting up global variables
 	var wins = 0;
-	//Create variable for losses
-	var losses = 0; 
-	//Create variable for counter
-	var counter = 0;
+	var losses = 0;
+	var numberToGuess = 0;
+	var crystalsAdded = 0;
+	var redCrystal = 0;
+	var blueCrystal = 0;
+	var yellowCrystal = 0;
+	var greenCrystal = 0;
 
-	//Edit HTML to update wins and losses within jQuery
-	$("#wins").html(wins);
-	$("#losses").html(losses);
-
-	randomCrystals();
-	newGame();
-
-	//Create function for all colored crystals)
+	function randomCrystalNumber() {
+		//Setting a random number between 1-12 for each crystal.
+		redCrystal = Math.ceil(Math.random()*12); 
+			console.log(redCrystal + " This is the random number for the redCrystal");
+		blueCrystal = Math.ceil(Math.random()*12);
+			console.log(blueCrystal + " This is the random number for blueCrystal");
+		yellowCrystal = Math.ceil(Math.random()*12);
+			console.log(yellowCrystal + " This is the random number for yellowCrystal");
+		greenCrystal = Math.ceil(Math.random()*12);
+			console.log(greenCrystal + " This is the random number for greenCrystal");
 		
-	function randomCrystals () {
-		//Create an empty array for colored crystals
-		var numberArray = []
-			//There are 4 colored crystals, hence why it is less than 4. Using a while loop.
-			//While loop will continue to be executed as long as the condition is true.
-			while(numberArray.length < 4){
-				//Spits out a random number with the smallest integer greater or equal to 12 b/c colored crystals can be no greater than 12.
-				var randomNumber = Math.ceil(Math.random()*12)
-				//Setting a boolean for the variable found
-				var found = false;
-				//Setting a for loop that specifies if [i] is less than 4, then increase [i] by 1. 
-				//When the numberArray index hits the random number then stop the loop.
-				for (var i = 0; i < numberArray.length; i++){
-					if (numberArray[i] == randomNumber){
-						found = true; break
-					}
-				}
-				//***I have no idea what this even means. 
-				if(!found)numberArray[numberArray.length]=randomNumber;
-			}
-		console.log(numberArray);
-
-		//Loop increasing [i] by one. 
-		//Changing the crystals in the HTML to reflect JS.
-		for (i=0; i < numberArray.length; i++){
-			var imgCrystal = $("<img>");
-			imgCrystal.attr("data-num", numberArray[i]);
-			imgCrystal.attr("src", crystals[i]);
-			imgCrystal.attr("alt", "crystals");
-			imgCrystal.addClass("crystalImage");
-			$("#crystals").append(imgCrystal);
-		}
 	}
 
-	function newGame () {
-		//Setting the counter variable to change in the #yourScore html
-		counter = 0;
-		$("#yourScore").html(counter);
-		//Creating a function that will spit out a random integer between min and max
-		function randomIntFromInterval(min,max){
-			//Returning a random number that is rounded down to the nearest whole number.
-			//***Subtracting the max from the min and adding 1? I don't know what this really means.
-			return Math.floor(Math.random()*(max-min+1)+min);
-		}
-		//Creating a new varible for user to guess.
-		//Setting the conditions for min 19 and max 120.
-		var guessThisNumber = randomIntFromInterval(19,120);
-		//Setting the class value in the HTML to change based upon the variable guessThisNumber
-		$(".value").html(guessThisNumber);
-		//Setting the class imgCrystal for an on click event
-		$("#crystals").on("click", function(){
-			//***The counter variable is now equal to the counter number plus... I don't know
-			//***We're parsing a string and turning it into an integer. 
-			counter = counter + parseInt($(this).data("num"));
-			//Setting the ID yourScore in the HTML to change based upon the varible counter
-			$("#yourScore").html(counter);
-			//Setting a condition if counter equals guessThisNumber then:
-			if (counter === guessThisNumber){
-				//Change counter ID in HTML to "Congrats..."
-				$("#counter").html("Congratulations! You won!");
-				//Increase wins count by 1.
-				wins++
-				//Change wins ID in HTML to wins variable
-				$("#wins").html(wins);
-				//console.logging the wins for debugging purposes.
-				console.log(wins);
-				//Change the crystals ID in HTML and clear random number associated with it.
-				$("#crystals").empty();
-				//Run two functions:
-				newCrystals();
-				newGame();
-			//Creating another condition
-			//If counter varible is greater than guessThisNumber then:
-			} else if (counter > guessThisNumber){
-				//Change counter ID to say "Sorry..."
-				$("#counter").html("Sorry! You lost! Try again.");
-				//Increases loss by 1.
-				losses++
-				//Change losses ID to reflect losses variable
-				$("losses").html(losses);
-				//console.logging losses variable
-				console.log(losses);
-				//Change crystals ID in HTML and change it to empty.
-				$("#crystals").empty();
-				//Run two functions:
-				newCrystals();
-				newGame();
-			}
-		});
+	function randomNumberToGuess(){
+		numberToGuess = Math.ceil(Math.random()*120-19)+19;
+		//Change the HTML
+		$(".value").html(numberToGuess);
+		console.log(numberToGuess + " This is the random number you are supposed to guess");
 	}
+
+	function checkWin(){
+		if(crystalsAdded === numberToGuess){
+			alert("Congratulations! You won! Way to go!")
+			wins++
+			$("#wins").html(wins);
+			randomCrystalNumber();
+			randomNumberToGuess();
+			crystalsAdded = 0;
+			$("#yourScore").html(crystalsAdded);
+		} 
+		else if(crystalsAdded > numberToGuess){
+			alert("You lost, loser!")
+			losses++
+			$("#losses").html(losses);
+			randomCrystalNumber();
+			randomNumberToGuess();
+			crystalsAdded = 0;
+			$("#yourScore").html(crystalsAdded);
+		}
+
+	}
+	$(".red").on("click", function(){
+		crystalsAdded = crystalsAdded + redCrystal;
+		$("#yourScore").html(crystalsAdded);
+		checkWin();
+		console.log(crystalsAdded + " This is crystalsAdded plus the redCrystal");
+	});
+
+	$(".blue").on("click", function(){
+		crystalsAdded = crystalsAdded + blueCrystal;
+		$("#yourScore").html(crystalsAdded);
+		checkWin();
+		console.log(crystalsAdded + " This is crystalsAdded plus the blueCrystal");
+	});
+
+	$(".yellow").on("click", function(){
+		crystalsAdded = crystalsAdded + yellowCrystal;
+		$("#yourScore").html(crystalsAdded);
+		checkWin();
+		console.log(crystalsAdded + " This is crystalsAdded plus the yellowCrystal");
+	});
+
+	$(".green").on("click", function(){
+		crystalsAdded = crystalsAdded + greenCrystal;
+		$("#yourScore").html(crystalsAdded);
+		checkWin();
+		console.log(crystalsAdded + " This is crystalsAdded plus the greenCrystal");
+	});
+
+
+
+	randomCrystalNumber();
+	randomNumberToGuess();
 });
+
+
+
+
+
+
+// On click event to generate random number for big crystal.
+
+// One click event to generate random number for small crystals, but keep the same number per game. Lock key?
+
+// One empty array for all of the crystals
+
+// On click event to push crystal num into empty array
+
+// Array is equal to random number variable equals a win 
+
+// Array is less than random number variable and is more than the attempts left equals a loss 
+
+// Clear empty array for crystals.
+
+// Reset attemps variable
+
+// Generate new random number associated with each crystal.
 		
 		
 	
